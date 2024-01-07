d545b7c2872e96e9bbeb28b9704bc41de190258f
Eduardo Bart <edub4rt@gmail.com>
2024-01-07 09:10:28 -0300

    Use per vertex coloring instead of uniform colors

--- shaders/sample-effect.glsl
+++ shaders/sample-effect.glsl
@@ -4,10 +4,13 @@
 
 @vs vs
 layout(location=0) in vec4 coord;
+layout(location=1) in vec4 color;
 layout(location=0) out vec2 texUV;
+layout(location=1) out vec4 iColor;
 void main() {
     gl_Position = vec4(coord.xy, 0.0, 1.0);
     texUV = coord.zw;
+    iColor = color;
 }
 @end
 
@@ -25,8 +28,9 @@ uniform uniforms {
     float iZoom;
     float iLevel;
 };
-in vec2 texUV;
-out vec4 fragColor;
+layout(location=0) in vec2 texUV;
+layout(location=1) in vec4 iColor;
+layout(location=0) out vec4 fragColor;
 float noise(vec2 p) {
     return texture(sampler2D(iTexChannel1, iSmpChannel1), p).r;
 }
@@ -35,7 +39,7 @@ void main() {
     vec2 fog_uv = (texUV * vec2(iRatio, 1.0)) * iZoom;
     float f = noise(fog_uv - iVelocity*iTime);
     f = noise(fog_uv + f*iWarpiness);
-    vec3 col = mix(tex_col, vec3(f), iPressure);
+    vec3 col = mix(tex_col, vec3(f) * iColor.rgb, iPressure);
     fragColor = vec4(col, 1.0);
 }
 @end
--- shaders/sokol_gp.glsl
+++ shaders/sokol_gp.glsl
@@ -2,22 +2,23 @@
 @module sgp
 
 @vs vs
-in vec4 coord;
-out vec2 texUV;
+layout(location=0) in vec4 coord;
+layout(location=1) in vec4 color;
+layout(location=0) out vec2 texUV;
+layout(location=1) out vec4 iColor;
 void main() {
     gl_Position = vec4(coord.xy, 0.0, 1.0);
     texUV = coord.zw;
+    iColor = color;
 }
 @end
 
 @fs fs
 uniform texture2D iTexChannel0;
 uniform sampler iSmpChannel0;
-uniform fs_params {
-    vec4 iColor;
-};
-in vec2 texUV;
-out vec4 fragColor;
+layout(location=0) in vec2 texUV;
+layout(location=1) in vec4 iColor;
+layout(location=0) out vec4 fragColor;
 void main() {
     fragColor = texture(sampler2D(iTexChannel0, iSmpChannel0), texUV) * iColor;
 }
